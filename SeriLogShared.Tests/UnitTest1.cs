e4a0c4267bf1d6daae8ea5a1b50c4d4e255854b7
zzt108 <10920613+zzt108@users.noreply.github.com>
2025-10-18 21:49:05 +0200

    feat: add SeriLogShared.Tests project to solution
    • TL;DR: Add new test project for SeriLog functionality to expand unit test coverage
    • Add SeriLogShared.Tests project reference to LogCtx.sln
    • Configure Debug and Release build configurations for test project
    • Enable parallel testing capabilities for SeriLog shared components
    • Support comprehensive testing strategy across logging frameworks (NLog + SeriLog)
    
    This change expands the testing infrastructure by adding a dedicated test project
    for SeriLog shared functionality, complementing the existing LogCtxShared.Tests
    project. The new project follows the established testing pattern and enables
    thorough validation of SeriLog-specific logging context features.

---
+++ SeriLogShared.Tests/UnitTest1.cs
@@ -0,0 +1,154 @@
+// tests/SeriLogShared.Tests/CtxLogger_LogLevel_Tests.cs
+
+using NLogShared;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+//using Serilog;
+//using Serilog.Core;
+//using Serilog.Events;
+using SeriLogShared;
+using Shouldly;
+using System;
+using System.Collections.Concurrent;
+using System.Linq;
+
+namespace SeriLogShared.Tests
+{
+    [TestFixture]
+    [Category("unit")]
+    [Parallelizable(ParallelScope.None)]
+    public class CtxLogger_LogLevel_Tests
+    {
+        private TestSink _sink;
+        private Logger _testLogger;
+        private CtxLogger _ctxLogger = new();
+
+        // private CtxLogger Log = new();
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _sink = new TestSink();
+            //_testLogger = new LoggerConfiguration()
+            //    .MinimumLevel.Verbose()
+            //    .WriteTo.Sink(_sink)
+            //    .CreateLogger();
+
+            //Log.CloseAndFlush();
+            //Log.Logger = _testLogger;
+
+            _ctxLogger = new CtxLogger();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _ctxLogger?.Dispose();
+        }
+
+        [Test]
+        public void Trace_Writes_Verbose_Level()
+        {
+            // Act
+            _ctxLogger.Trace("trace message");
+
+            // Assert
+            var evt = _sink.Events.LastOrDefault();
+            evt.ShouldNotBeNull();
+            evt.Level.ShouldBe(LogEventLevel.Verbose);
+            evt.RenderMessage().ShouldBe("trace message");
+            evt.Exception.ShouldBeNull();
+        }
+
+        [Test]
+        public void Debug_Writes_Debug_Level()
+        {
+            // Act
+            _ctxLogger.Debug("debug message");
+
+            // Assert
+            var evt = _sink.Events.LastOrDefault();
+            evt.ShouldNotBeNull();
+            evt.Level.ShouldBe(LogEventLevel.Debug);
+            evt.RenderMessage().ShouldBe("debug message");
+            evt.Exception.ShouldBeNull();
+        }
+
+        [Test]
+        public void Info_Writes_Information_Level()
+        {
+            // Act
+            _ctxLogger.Info("info message");
+
+            // Assert
+            var evt = _sink.Events.LastOrDefault();
+            evt.ShouldNotBeNull();
+            evt.Level.ShouldBe(LogEventLevel.Information);
+            evt.RenderMessage().ShouldBe("info message");
+            evt.Exception.ShouldBeNull();
+        }
+
+        [Test]
+        public void Warn_Writes_Warning_Level()
+        {
+            // Act
+            _ctxLogger.Warn("warn message");
+
+            // Assert
+            var evt = _sink.Events.LastOrDefault();
+            evt.ShouldNotBeNull();
+            evt.Level.ShouldBe(LogEventLevel.Warning);
+            evt.RenderMessage().ShouldBe("warn message");
+            evt.Exception.ShouldBeNull();
+        }
+
+        [Test]
+        public void Error_Writes_Error_Level_With_Exception()
+        {
+            // Arrange
+            var ex = new InvalidOperationException("boom");
+
+            // Act
+            _ctxLogger.Error(ex, "error message");
+
+            // Assert
+            var evt = _sink.Events.LastOrDefault();
+            evt.ShouldNotBeNull();
+            evt.Level.ShouldBe(LogEventLevel.Error);
+            evt.RenderMessage().ShouldBe("error message");
+            evt.Exception.ShouldNotBeNull();
+            evt.Exception.Message.ShouldContain("boom");
+        }
+
+        [Test]
+        public void Fatal_Writes_Fatal_Level_With_Exception()
+        {
+            // Arrange
+            var ex = new ApplicationException("fatality");
+
+            // Act
+            _ctxLogger.Fatal(ex, "fatal message");
+
+            // Assert
+            var evt = _sink.Events.LastOrDefault();
+            evt.ShouldNotBeNull();
+            evt.Level.ShouldBe(LogEventLevel.Fatal);
+            evt.RenderMessage().ShouldBe("fatal message");
+            evt.Exception.ShouldNotBeNull();
+            evt.Exception.Message.ShouldContain("fatality");
+        }
+
+        private sealed class TestSink : ILogEventSink
+        {
+            private readonly ConcurrentQueue<LogEvent> _events = new ConcurrentQueue<LogEvent>();
+
+            public void Emit(LogEvent logEvent)
+            {
+                _events.Enqueue(logEvent);
+            }
+
+            public LogEvent[] Events => _events.ToArray();
+        }
+    }
+}
